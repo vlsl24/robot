994bdfc97b1202c3397f2a4c0807151131b479bf
Pesjier <101105005+Pesjier@users.noreply.github.com>
2024-10-08 11:08:18 +0200

    Update main.py
    Typ versionen vi hade vid labb 1

--- main.py
+++ main.py
@@ -1,57 +1,83 @@
 #!/usr/bin/env pybricks-micropython
 
-"""
-Example LEGO® MINDSTORMS® EV3 Robot Educator Color Sensor Down Program
-----------------------------------------------------------------------
-
-This program requires LEGO® EV3 MicroPython v2.0.
-Download: https://education.lego.com/en-us/support/mindstorms-ev3/python-for-ev3
-
-Building instructions can be found at:
-https://education.lego.com/en-us/support/mindstorms-ev3/building-instructions#robot
-"""
-
-from pybricks.ev3devices import Motor, ColorSensor
-from pybricks.parameters import Port
-from pybricks.tools import wait
+# Pybricks imports
+from pybricks import robotics
+from pybricks.hubs import EV3Brick
+from pybricks.ev3devices import Motor, ColorSensor, UltrasonicSensor
+from pybricks.parameters import Port, Stop, Direction, Color
+from pybricks.tools import wait, StopWatch
 from pybricks.robotics import DriveBase
 
-# Initialize the motors.
+# Robot definitions
+ev3 = EV3Brick()
+
+# Motor definitions
 left_motor = Motor(Port.B)
 right_motor = Motor(Port.C)
-
-# Initialize the color sensor.
-line_sensor = ColorSensor(Port.S3)
-
-# Initialize the drive base.
 robot = DriveBase(left_motor, right_motor, wheel_diameter=55.5, axle_track=104)
 
-# Calculate the light threshold. Choose values based on your measurements.
-BLACK = 9
-WHITE = 85
-threshold = (BLACK + WHITE) / 2
+# Sensor definitions
+left_light = ColorSensor(Port.S2)
+right_light = ColorSensor(Port.S3)
+distance_sensor = UltrasonicSensor(Port.S4) 
 
-# Set the drive speed at 100 millimeters per second.
-DRIVE_SPEED = 100
+"""
+Important! The code may not function correctly if the first line from the template is not included.
+"""
 
-# Set the gain of the proportional line controller. This means that for every
-# percentage point of light deviating from the threshold, we set the turn
-# rate of the drivebase to 1.2 degrees per second.
+# Your code goes here
 
-# For example, if the light value deviates from the threshold by 10, the robot
-# steers at 10*1.2 = 12 degrees per second.
-PROPORTIONAL_GAIN = 1.2
+# Define line/background reflection
+line_reflection = 7
+background_reflection = 60
 
-# Start following the line endlessly.
-while True:
-    # Calculate the deviation from the threshold.
-    deviation = line_sensor.reflection() - threshold
+#Creating target value, this will be the value when the line/background both share the same amount of the sensor
+target_value = (line_reflection + background_reflection) * 0.5
 
-    # Calculate the turn rate.
-    turn_rate = PROPORTIONAL_GAIN * deviation
+#Abs amount from line reflection -> target and background reflection -> target
+difference = target_value - line_reflection
 
-    # Set the drive base speed and turn rate.
-    robot.drive(DRIVE_SPEED, turn_rate)
+rotation_speed = 25.0
+drive_speed = 25.0
 
-    # You can wait for a short time or do other things in this loop.
-    wait(10)
+found_line = False
+
+def follow_line():
+    global waitedTime
+    global found_line
+
+    while(not found_line):
+        robot.turn(90)
+        robot.stop()
+        robot.drive(100, 0)
+        waitedTime = 0
+        while(waitedTime < 2000):
+            wait(10)
+        
+            if(right_light.reflection() <= line_reflection):
+                found_line = True
+        
+            waitedTime += 10
+
+
+    #Found line
+    while True:
+        reflection = right_light.reflection()
+
+        #Because we do 'reflection - target_value' we are following the left side of the line
+        offset_to_target = reflection - target_value
+
+        #Convert offset to a value between -1.0 -> 1.0
+        rotation_percentage = offset_to_target / difference
+
+        if(abs(rotation_percentage) > 0.5):
+            robot.drive(0, rotation_speed * rotation_percentage)
+        else:
+            robot.drive(drive_speed, rotation_speed * rotation_percentage)  
+
+
+follow_line()
+
+# If we need to get the reflections of line/background
+# while True:
+#     print(sensor.reflection())
